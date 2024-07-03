4864f95bd15b57d01f02b8b38837346211592a8e
KMurray6416 <kmurray6416@gmail.com>
2024-07-03 02:07:05 -0600

    Task 2 Commit

---
+++ Dance.py
@@ -0,0 +1,7 @@
+weather = "rainy"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+
+else:
+     print("Take an umbrella!")
---
+++ HappyBlues.py
@@ -0,0 +1,8 @@
+user_mood = input("Hello! How are You feeling today?:")
+
+if  user_mood == "happy":
+    print ("That's great to hear!")
+
+elif user_mood == "sad":
+    print ("I hope your day gets better!")
+    
